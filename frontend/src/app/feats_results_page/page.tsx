b163f425fe7a79719eb56797e66be5ccdea4d7d3
Squeemble <jbrockmann@ewe.net>
2025-01-28 13:46:40 +0100

    built basic structure to display individual athletes

--- frontend/src/app/feats_results_page/page.tsx
+++ frontend/src/app/feats_results_page/page.tsx
@@ -1,16 +1,23 @@
+"use client";
 import Image from "next/image";
 import styles from "../page.module.css";
 import { json } from "stream/consumers";
 import exp from "constants";
+import { useSearchParams } from "next/navigation";
 
 
 export default function FeatsResultsPage() {
+  const searchParams = useSearchParams();
+  const presentID=searchParams.get('id');
   return (
     <div className={styles.page}>
       <main className={styles.main}>
       <h1 className={styles.title}>Leistungen Ergebnisse</h1>
       <section className={styles.resultsSection}>
         <p>Hier sind die Ergebnisse und Leistungen:</p>
+        <p>
+        Displaying for ID: {presentID}
+        </p>
       </section>
       </main>
       <footer className={styles.footer}>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,28 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import { json } from "stream/consumers";
 import exp from "constants";
 import Link from "next/link";
+import { useState } from "react";
 
 
 export default function Home() {
+  const [queryValue, setQueryValue] = useState("");
   return (
     <div className={styles.page}>
       <main className={styles.main}>
        Nur hier um zu existieren, gerne schnell überschreiben
-       <Link href="/feats_results_page">Anzeige der Ergebnisse</Link>
+       <div>
+        <label htmlFor="dynamicInput">Enter something:</label>
+        <input
+          id="dynamicInput"
+          type="text"
+          value={queryValue}
+          onChange={(e) => setQueryValue(e.target.value)}
+        />
+      </div>
+       <Link href={`/feats_results_page?id=${encodeURIComponent(queryValue)}`}>Anzeige der Ergebnisse</Link>
       </main>
       <footer className={styles.footer}>
       </footer>
