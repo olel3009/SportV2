96a83c246abf6c172810ec118b9fff4bfa53e509
Squeemble <155956907+Squeemble@users.noreply.github.com>
2025-01-29 10:51:22 +0100

    Added first file for generic functions

---
+++ frontend/generic_functions/athlete_getters.tsx
@@ -0,0 +1,68 @@
+//Fügen Sie hier alle Funktionen ein, die Athleten abrufen, damit sie im bereits vorhandenen Code verwendet werden können
+//Auf diese Weise müssen wir, wenn die API fertig ist, nur die Logik hier ändern und nicht an anderer Stelle im Code
+//Außerdem werden diese Funktionen mit ziemlicher Sicherheit mehr als einmal verwendet, daher ist es gut, sie woanders zu platzieren
+
+let mockupData = `[
+    {
+      "id": 420,
+      "lastName": "Schulz",
+      "name": "Dieter",
+      "sex": "m",
+      "dateOfBirth": "18.03.2014",
+      "disciplines": ["Sprinten", "Springen"],
+      "feats": [
+        {
+          "discipline": "Sprinten",
+          "date": "5.09.2023",
+          "result": "10 Sek",
+          "score": "90"
+        },
+        {
+          "discipline": "Sprinten",
+          "date": "5.09.2022",
+          "result": "90 Sek",
+          "score": "1"
+        },
+        {
+          "discipline": "Springen",
+          "date": "5.08.2023",
+          "result": "50 cm",
+          "score": "70"
+        }
+      ]
+    },
+    {
+      "id": 69,
+      "lastName": "Dortmeier",
+      "name": "Peter",
+      "sex": "m",
+      "dateOfBirth": "10.08.2010",
+      "disciplines": ["Sprinten", "Springen"],
+      "feats": [
+        {
+          "discipline": "Springen",
+          "date": "5.08.2023",
+          "result": "90 cm",
+          "score": "80"
+        },
+        {
+          "discipline": "Sprinten",
+          "date": "5.09.2023",
+          "result": "5 Sek",
+          "score": "100"
+        },
+        {
+          "discipline": "Sprinten",
+          "date": "5.09.2022",
+          "result": "10 Sek",
+          "score": "80"
+        }
+      ]
+    }
+  ]`;
+
+function getAthleteById(id: number) {  
+    let translatedTrainees = JSON.parse(mockupData);
+    // Return the trainee that has the id from the passed parameter 
+    return translatedTrainees.find(trainee => trainee.id === id);
+}
