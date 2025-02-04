c0f774bfd2e51e824051f02f7e84cf452766ce06
Squeemble <jbrockmann@ewe.net>
2025-02-04 11:29:49 +0100

    Wrote test for menu and made active site highlighted

--- frontend/generic_functions/base_layout.tsx
+++ frontend/generic_functions/base_layout.tsx
@@ -1,12 +1,18 @@
-// components/Layout.js
+"use client"
 import React, { ReactNode } from 'react'
 import Link from "next/link";
 import styles from "../src/app/page.module.css";
+import { usePathname, useSearchParams } from 'next/navigation'
+
 
 let sites: string[][] = [["Startseite", "/"], ["Testseite", "/test_page"]];
 
 function Nav_Menu() {
+  const path = usePathname();
   let links = sites.map((site, index) => {
+    if(site[1]==path){
+      return<Link href={site[1]} key={index} className={styles.navbutt_active}>{site[0]}</Link>
+    }
     return<Link href={site[1]} key={index} className={styles.navbutt}>{site[0]}</Link>
   });
   return <nav key = 'navMen' className={styles.navbar}>{links}</nav>;
