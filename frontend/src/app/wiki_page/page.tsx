1ed3a5dda42e7f94b9c023761910953b36fbd79c
Templer08 <lucas.alisch@stud-provadis-hochschule.de>
2025-03-03 10:54:27 +0100

    Hinzufügen von Silenium Tests

--- frontend/src/app/wiki_page/page.tsx
+++ frontend/src/app/wiki_page/page.tsx
@@ -11,8 +11,9 @@ export default function Home() {
           <li><a id="link_leistungen_ergebnisse_athlet_diziplin" href="#leistungen_ergebnisse_athlet_diziplin">Ansicht von Leistungen und Ergebnissen pro Athlet und Disziplin</a></li>
           <li><a id="link_detailansicht_athleten" href="#detailansicht_athleten">Detailansicht eines Athleten</a></li>
           <li><a id="link_eintragsmodus_leistungen" href="#eintragsmodus_leistungen">Eintragsmodus für Leistungswerte</a></li>
-          <li><a id="link_erstellen_ändern_von_reglungen" href="#erstellen_ändern_von_reglungen">Erstellen oder Ändern von Reglungen</a></li>
+          <li><a id="link_erstellen_aendern_von_reglungen" href="#erstellen_aendern_von_reglungen">Erstellen oder Ändern von Reglungen</a></li>
           <li><a id="link_export_eines_athleten" href="#export_eines_athleten">Export eines Athleten</a></li>
+          <li><a id="link_export_eines_athleten_pdf" href="#export_eines_athleten_pdf">Export eines Athleten und seiner Daten als PDF</a></li>
           <li><a id="link_export_mehrer_athlethen" href="#export_mehrer_athlethen">Export mehrerer Athleten als CSV</a></li>
           <li><a id="link_liste_athlethen" href="#liste_athlethen">Liste aller Athleten</a></li>
           <li><a id="link_knopf_reglungsaktualisierung" href="#knopf_reglungsaktualisierung">Manuelle Aktualisierung der Reglungen durch Knopfdruck</a></li>
@@ -21,7 +22,7 @@ export default function Home() {
           <li><a id="link_visuelle_darstellung_entwicklung" href="#visuelle_darstellung_entwicklung">Visuelle Darstellung der Entwicklung eines Athleten</a></li>
         </ul>
       </nav>
-      <div className={styles.content}>
+      <div className={styles.content2}>
         <h1>Wiki-Seite</h1>
         <div id="leistungen_ergebnisse_athlet_diziplin" className={styles.section}>
           <h2>Ansicht von Leistungen und Ergebnissen pro Athlet und Disziplin</h2>
@@ -35,12 +36,16 @@ export default function Home() {
           <h2>Eintragsmodus für Leistungswerte</h2>
           <p>Hier könnte die Beschreibung stehen</p>
         </div>
-        <div id="erstellen_ändern_von_reglungen" className={styles.section}>
+        <div id="erstellen_aendern_von_reglungen" className={styles.section}>
           <h2>Erstellen oder Ändern von Reglungen</h2>
           <p>Hier könnte die Beschreibung stehen</p>
         </div>
         <div id="export_eines_athleten" className={styles.section}>
           <h2>Export eines Athleten</h2>
+          <p>Hier könnte die Beschreibung stehen</p>
+        </div>
+        <div id="export_eines_athleten_pdf" className={styles.section}>
+          <h2>Export eines Athleten und seiner Daten als PDF</h2>
           <p>Hier könnte die Beschreibung stehen</p>
         </div>
         <div id="export_mehrer_athlethen" className={styles.section}>
--- frontend/tests/test_wiki.py
+++ frontend/tests/test_wiki.py
@@ -16,27 +16,53 @@ class wiki_test(unittest.TestCase):
         
         # Initialize the WebDriver with the options
         self.driver = webdriver.Chrome(options=chrome_options)
-        self.driver = webdriver.Chrome()
+        
         
         # Navigate to your target URL but fail the test if it takes too long
-        self.driver.set_page_load_timeout(10)
-        self.driver.get("http://localhost:3000/wikipage")
-        self.sites =[["Startseite", "/"], ["Testseite", "/test_page"],["Wiki-Seite", "/wiki_page"]]
+        self.driver.set_page_load_timeout(100)
+        
+        
     
     def test_nav_menu(self):
-
+        self.driver.get("http://localhost:3000")
+        self.sites =[["Startseite", "/"], ["Testseite", "/test_page"],["Wiki-Seite", "/wiki_page"]]
         for thing in self.sites:
             # Find the link to the page
             link = self.driver.find_element(By.LINK_TEXT, thing[0])
             link.click()
-            sleep(10)
+            sleep(5)
             self.assertEqual(self.driver.current_url, "http://localhost:3000"+thing[1], "The link did not lead to the right page!")
             print("Navigated to "+thing[0]+"!")
 
-def tearDown(self):
+
+    def test_wiki_page(self):
+     
+        # Find the link to the page
+        self.driver.get("http://localhost:3000/wiki_page")
+        self.sites =[["Ansicht von Leistungen und Ergebnissen pro Athlet und Disziplin", "#leistungen_ergebnisse_athlet_diziplin"], ["Detailansicht eines Athleten","#detailansicht_athleten"],
+                     ["Eintragsmodus für Leistungswerte","#eintragsmodus_leistungen"], ["Erstellen oder Ändern von Reglungen","#erstellen_aendern_von_reglungen"],
+                     ["Export eines Athleten","#export_eines_athleten"],["Export eines Athleten und seiner Daten als PDF","#export_eines_athleten_pdf"],
+                     ["Export mehrerer Athleten als CSV","#export_mehrer_athlethen"],["Liste aller Athleten","#liste_athlethen"], 
+                     ["Manuelle Aktualisierung der Reglungen durch Knopfdruck","#knopf_reglungsaktualisierung"],["Medaillen Ansicht","#medaillen_ansicht"], ["Regelungsverwaltung","#regelungsverwaltung"],
+                     ["Visuelle Darstellung der Entwicklung eines Athleten","#visuelle_darstellung_entwicklung"]]
+        for thing in self.sites:
+            # Find the link to the page
+            link = self.driver.find_element(By.LINK_TEXT, thing[0])
+            sleep(5)
+            link.click()
+            sleep(10)
+            self.assertEqual(self.driver.current_url, "http://localhost:3000/wiki_page"+thing[1], "The link did not lead to the right page!")
+            print("Navigated to "+thing[0]+"!")
+        
+        
+        
+
+
+    def tearDown(self):
         # Close the browser
         self.driver.quit()
         print("Test Wiki Success!")
+
 
 
 if __name__ == "__main__":
