4de65f3c0eb58dbb2692bb6e0dcc55dcacc32371
mark-alexander.mueller@telekom.de <mark-alexander.mueller@telekom.de>
2025-03-13 14:26:27 +0100

    logger

--- api/logs/logger.py
+++ api/logs/logger.py
@@ -1,33 +1,33 @@
 import logging
-import logging.handlers
+from logging.handlers import TimedRotatingFileHandler
 from flask import app
 import sys, os
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, parent_dir)
 import export_pdf
 
+
+formatter= logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+
+handler = TimedRotatingFileHandler(r'api\logs\system.log', when='midnight', backupCount= 3, interval=1)
+handler.setFormatter(formatter)
+
 logger = logging.getLogger(__name__)
-
-logging.basicConfig(
-    filename=r'api\logs\system.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
-    filemode = 'w',
-    )
-
-logging.handlers.TimedRotatingFileHandler('system.log', when='s', interval=10,)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 #@app.before_request
 def log_request_info():
-    #logging.info(f"Request: {request.method} {request.url} - Data: {request.json}")
+    #logger.info(f"Request: {request.method} {request.url} - Data: {request.json}")
     pass
 
-def log_info_test():
+def log_info_pdf():
     export_pdf.fill_out_fields(export_pdf.athlet1)
-    logging.info(f"PDF fuer den Athleten {export_pdf.athlet1.surname} wurde angelegt!")
+    logger.info(f"PDF eines Athleten wurde angelegt!")
 
 #logging modul erstellen zum import für jede benötigte Datei
 #keine personenbezogene Daten loggen nur die Veränderungen
 
 if __name__ == "__main__":
-    log_info_test()
+    while True:
+        log_info_pdf()
