4a70e8eff8c7a76b306fa178a7f512ab1c73eea1
mark-alexander.mueller@telekom.de <mark-alexander.mueller@telekom.de>
2025-04-04 11:14:23 +0200

    first try of the group api

--- api/routes/gruppe.py
+++ api/routes/gruppe.py
@@ -1,10 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Blueprint
+from database import db
+from database.models import Athlete as DBAthlete
+
 import athlete
 
 app = Flask(__name__)
 
-@app.route('/gruppen/export/pdf', methods='[GET]')
+@app.route('/gruppen/export/pdf', methods=['GET'])
 def group():
     group = []
-    for ath in group:
-        return jsonify(ath)
+    return jsonify([{
+        "id": athlete.id,
+        "first_name": athlete.first_name,
+        "last_name": athlete.last_name,
+        "birth_date": athlete.birth_date.strftime('%d-%m-%Y'),
+        "gender": athlete.gender,
+        "created_at": athlete.created_at,
+        "updated_at": athlete.updated_at
+    }for athlete in group])
