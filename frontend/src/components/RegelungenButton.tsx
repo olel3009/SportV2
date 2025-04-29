c26ba8a8c8df2b05775c16a59462e4dd2c2ece9b
Templer08 <lucas.alisch@stud-provadis-hochschule.de>
2025-04-29 14:23:28 +0200

    Leichte Verbesseung an den Tooltipps

--- frontend/src/components/RegelungenButton.tsx
+++ frontend/src/components/RegelungenButton.tsx
@@ -63,68 +63,54 @@ export default function RegelungenButton() {
         setUploadedFile(file);
         setErrorMessage(""); // Entfernt vorherige Fehlermeldungen
       } else {
-
         setErrorMessage("Bitte laden Sie eine gültige CSV-Datei hoch."); // Fehlermeldung für ungültige Dateien
       }
     }
   };
-
   const handleYearChange = (year: string) => {
     setSelectedYear(year);
   };
-
   return (
     <div>
       {button_loggig_color() === wert ? (
         <div>
-
-
-
-          <Tooltip.Provider>
-            <Tooltip.Root>
-              <Tooltip.Trigger asChild>
-                <Button variant="default" onClick={handleButtonClick}>
-                  Regelungen aktualisieren
-                </Button>
-
-              </Tooltip.Trigger>
-              <p dangerouslySetInnerHTML={{ __html: buttonresult }}></p>
-              <Tooltip.Content
-                side="right" // Tooltip wird rechts angezeigt
-                align="center" // Zentriert den Tooltip vertikal zur Maus
-                sideOffset={10} // Abstand zwischen Tooltip und Maus
-                className="bg-gray-800 text-white text-sm px-2 py-1 rounded shadow-md max-w-xs break-words"
-              >
-                Klicken Sie hier, um die Regelungen zu aktualisieren
-                <Tooltip.Arrow className="fill-gray-800" />
-              </Tooltip.Content>
-            </Tooltip.Root>
-          </Tooltip.Provider>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>
+              <Button variant="default" onClick={handleButtonClick}>
+                Regelungen aktualisieren
+              </Button>
+            </Tooltip.Trigger>
+            <p dangerouslySetInnerHTML={{ __html: buttonresult }}></p>
+            <Tooltip.Content
+              side="right" // Tooltip wird rechts angezeigt
+              align="center" // Zentriert den Tooltip vertikal zur Maus
+              sideOffset={10} // Abstand zwischen Tooltip und Maus
+              className="bg-gray-800 text-white text-sm px-2 py-1 rounded shadow-md max-w-xs break-words"
+            >
+              Klicken Sie hier, um die Regelungen zu aktualisieren
+              <Tooltip.Arrow className="fill-gray-800" />
+            </Tooltip.Content>
+          </Tooltip.Root>
         </div>
-
-
       ) : (
         <div>
-          <Tooltip.Provider>
-            <Tooltip.Root>
-              <Tooltip.Trigger asChild>
-                <Button variant="destructive" onClick={handleButtonClick}>
-                  Regelungen aktualisieren
-                </Button>
-
-              </Tooltip.Trigger>
-              <p dangerouslySetInnerHTML={{ __html: buttonresult }}></p>
-              <Tooltip.Content
-                side="right" // Tooltip wird rechts angezeigt
-                align="center" // Zentriert den Tooltip vertikal zur Maus
-                sideOffset={10} // Abstand zwischen Tooltip und Maus
-                className="bg-gray-800 text-white text-sm px-2 py-1 rounded shadow-md max-w-xs break-words"
-              >
-                Klicken Sie hier, um die Regelungen zu aktualisieren
-                <Tooltip.Arrow className="fill-gray-800" />
-              </Tooltip.Content>
-            </Tooltip.Root>
-          </Tooltip.Provider>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>
+              <Button variant="destructive" onClick={handleButtonClick}>
+                Regelungen aktualisieren
+              </Button>
+            </Tooltip.Trigger>
+            <p dangerouslySetInnerHTML={{ __html: buttonresult }}></p>
+            <Tooltip.Content
+              side="right" // Tooltip wird rechts angezeigt
+              align="center" // Zentriert den Tooltip vertikal zur Maus
+              sideOffset={10} // Abstand zwischen Tooltip und Maus
+              className="bg-gray-800 text-white text-sm px-2 py-1 rounded shadow-md max-w-xs break-words"
+            >
+              Klicken Sie hier, um die Regelungen zu aktualisieren
+              <Tooltip.Arrow className="fill-gray-800" />
+            </Tooltip.Content>
+          </Tooltip.Root>
         </div>
       )}
       <Dialog
@@ -137,79 +123,65 @@ export default function RegelungenButton() {
           <DialogHeader>
             <DialogTitle>Regelungen aktualisieren</DialogTitle>
           </DialogHeader>
-
-
           {/* Drag-and-Drop-Bereich */}
-
-
-          <Tooltip.Provider>
-            <Tooltip.Root>
-              <Tooltip.Trigger asChild>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>
               <div
-            className="relative border border-dashed border-gray-300 p-4 rounded-md mb-4 cursor-pointer flex flex-col items-center justify-center h-48"
-            onClick={() => fileInputRef.current?.click()}
-            onDragOver={(e) => {
-              e.preventDefault(); // Verhindert das Standardverhalten des Browsers
-              e.stopPropagation();
-              e.dataTransfer.dropEffect = "copy"; // Zeigt an, dass Dateien kopiert werden können
-            }}
-            onDrop={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-
-              if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-                const file = e.dataTransfer.files[0];
-                if (file.name.endsWith(".csv")) {
-                  setUploadedFile(file);
-                  setErrorMessage(""); // Entfernt vorherige Fehlermeldungen
-                } else {
-                  setErrorMessage("Bitte laden Sie eine gültige CSV-Datei hoch.");
-                }
-
-              }
-            }}
-          >
-            <FileUp className="absolute w-40 h-40 text-gray-200" />
-            {uploadedFile ? (
-              <p className="z-10">
-                Hochgeladene Datei: <strong>{uploadedFile.name}</strong> <br />
-                Falls Sie eine andere Datei hochladen möchten, ziehen Sie diese hierher oder klicken Sie auf das Feld. <br />
-                Die alte Datei wird mit der neuen Datei ersetzt.
-              </p>
-            ) : (
-              <div className="text-center z-10">
-                <p>
-                  Ziehen Sie eine CSV-Datei hierher oder klicken Sie, um eine Datei auszuwählen. <br />
-                  Es ist nur eine Datei gleichzeitig möglich.
-                </p>
-              </div>
-            )}
-            <input
-              type="file"
-              accept=".csv"
-              onChange={handleFileChange}
-              className="hidden"
-              ref={fileInputRef}
-            />
-          </div>
-
-              </Tooltip.Trigger>
-              
-              <Tooltip.Content
-                side="left" // Tooltip wird rechts angezeigt
-                align="center" // Zentriert den Tooltip vertikal zur Maus
-                sideOffset={10} // Abstand zwischen Tooltip und Maus
-                className="bg-gray-800 text-white text-sm px-2 py-1 rounded shadow-md max-w-xs break-words"
+                className="relative border border-dashed border-gray-300 p-4 rounded-md mb-4 cursor-pointer flex flex-col items-center justify-center h-48"
+                onClick={() => fileInputRef.current?.click()}
+                onDragOver={(e) => {
+                  e.preventDefault(); // Verhindert das Standardverhalten des Browsers
+                  e.stopPropagation();
+                  e.dataTransfer.dropEffect = "copy"; // Zeigt an, dass Dateien kopiert werden können
+                }}
+                onDrop={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+                    const file = e.dataTransfer.files[0];
+                    if (file.name.endsWith(".csv")) {
+                      setUploadedFile(file);
+                      setErrorMessage(""); // Entfernt vorherige Fehlermeldungen
+                    } else {
+                      setErrorMessage("Bitte laden Sie eine gültige CSV-Datei hoch.");
+                    }
+                  }
+                }}
               >
-                Hier Klicken um Datei Explorer zu öffnen, um eine Datei hochzuladen. 
-                <Tooltip.Arrow className="fill-gray-800" />
-              </Tooltip.Content>
-            </Tooltip.Root>
-          </Tooltip.Provider>
-
-
-          
-
+                <FileUp className="absolute w-40 h-40 text-gray-200" />
+                {uploadedFile ? (
+                  <p className="z-10">
+                    Hochgeladene Datei: <strong>{uploadedFile.name}</strong> <br />
+                    Falls Sie eine andere Datei hochladen möchten, ziehen Sie diese hierher oder klicken Sie auf das Feld. <br />
+                    Die alte Datei wird mit der neuen Datei ersetzt.
+                  </p>
+                ) : (
+                  <div className="text-center z-10">
+                    <p>
+                      Ziehen Sie eine CSV-Datei hierher oder klicken Sie, um eine Datei auszuwählen. <br />
+                      Es ist nur eine Datei gleichzeitig möglich.
+                    </p>
+                  </div>
+                )}
+                <input
+                  type="file"
+                  accept=".csv"
+                  onChange={handleFileChange}
+                  className="hidden"
+                  ref={fileInputRef}
+                />
+              </div>
+            </Tooltip.Trigger>
+            <Tooltip.Content
+              side="left" // Tooltip wird rechts angezeigt
+              align="center" // Zentriert den Tooltip vertikal zur Maus
+              sideOffset={10} // Abstand zwischen Tooltip und Maus
+              className="bg-gray-800 text-white text-sm px-2 py-1 rounded shadow-md max-w-xs break-words"
+            >
+              Hier Klicken um Datei Explorer zu öffnen, um eine Datei hochzuladen.
+              <Tooltip.Arrow className="fill-gray-800" />
+            </Tooltip.Content>
+          </Tooltip.Root>
           <div className="mb-4">
             <label className="block text-base font-medium text-gray-700 mb-2">
               Jahr auswählen:
@@ -229,19 +201,13 @@ export default function RegelungenButton() {
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
-
           {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
-
           <DialogFooter>
-
-
-          <Tooltip.Provider>
             <Tooltip.Root>
               <Tooltip.Trigger asChild>
-              <Button variant="destructive" onClick={handleClosePopup}>
-              Abbrechen
-            </Button>
-
+                <Button variant="destructive" onClick={handleClosePopup}>
+                  Abbrechen
+                </Button>
               </Tooltip.Trigger>
               <Tooltip.Content
                 side="left" // Tooltip wird rechts angezeigt
@@ -249,20 +215,15 @@ export default function RegelungenButton() {
                 sideOffset={10} // Abstand zwischen Tooltip und Maus
                 className="bg-gray-800 text-white text-sm px-2 py-1 rounded shadow-md max-w-xs break-words"
               >
-                Hier Klicken um den Vorgang Abzubrechen. 
+                Hier Klicken um den Vorgang Abzubrechen.
                 <Tooltip.Arrow className="fill-gray-800" />
               </Tooltip.Content>
             </Tooltip.Root>
-          </Tooltip.Provider>
-
-            
-          <Tooltip.Provider>
             <Tooltip.Root>
               <Tooltip.Trigger asChild>
-              <Button variant="default" className="bg-green-500 hover:bg-green-600 text-white" onClick={handleConfirm}>
-              Bestätigen
-            </Button>
-
+                <Button variant="default" className="bg-green-500 hover:bg-green-600 text-white" onClick={handleConfirm}>
+                  Bestätigen
+                </Button>
               </Tooltip.Trigger>
               <Tooltip.Content
                 side="right" // Tooltip wird rechts angezeigt
@@ -270,15 +231,10 @@ export default function RegelungenButton() {
                 sideOffset={10} // Abstand zwischen Tooltip und Maus
                 className="bg-gray-800 text-white text-sm px-2 py-1 rounded shadow-md max-w-xs break-words"
               >
-                Hier Klicken um den Vorgang des Aktualisierens zu starten. 
+                Hier Klicken um den Vorgang des Aktualisierens zu starten.
                 <Tooltip.Arrow className="fill-gray-800" />
               </Tooltip.Content>
             </Tooltip.Root>
-          </Tooltip.Provider>
-          
-
-
-            
           </DialogFooter>
         </DialogContent>
       </Dialog>
