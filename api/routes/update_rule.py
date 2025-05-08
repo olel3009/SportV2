794ce11c7f3427050bbfc8daadf806a9bc039a36
mark-alexander.mueller@telekom.de <mark-alexander.mueller@telekom.de>
2025-05-08 11:34:15 +0200

    Grundlage fertig

--- api/routes/update_rule.py
+++ api/routes/update_rule.py
@@ -3,8 +3,9 @@ from database.models import Rule as DBRule
 from database import db
 import logging
 import csv
+from sqlalchemy import select
 
-bp_update = Blueprint("update rule", __name__)
+bp_update = Blueprint("update-rule", __name__)
 
 @bp_update.route("/regelungen/aktualisieren", methods = ["POST"])
 def update_rule(csv_updates):
@@ -13,19 +14,21 @@ def update_rule(csv_updates):
             reader = csv.reader(csv_data)
             rules = DBRule.query.all()
             for row in reader:
-                discilplin = row["Disziplin"]
-                min_age = row["Altersgruppe"].split("-")[0]
-                max_age = row["Altersgruppe"].split("-")[1]
+                rule_id = select("Rule").where(rules.disciplin == row["Disziplin"])
+                dbrule= DBRule.query.get_or_404(rule_id)
+                dbrule.discilplin = row["Disziplin"]
+                dbrule.min_age = row["Altersgruppe"].split("-")[0]
+                dbrule.max_age = row["Altersgruppe"].split("-")[1]
                 
                 gender = row["Geschlecht"]
                 if gender == "männlich":
-                    treshold_bronze_m = row["Bronze"]
-                    treshold_silver_m = row["Silber"]
-                    treshold_gold_m = row["Gold"]
+                    dbrule.treshold_bronze_m = row["Bronze"]
+                    dbrule.treshold_silver_m = row["Silber"]
+                    dbrule.treshold_gold_m = row["Gold"]
                 else:
-                    treshold_bronze_f = row["Bronze"]
-                    treshold_silver_f = row["Silber"]
-                    treshold_gold_f = row["Gold"]
+                    dbrule.treshold_bronze_f = row["Bronze"]
+                    dbrule.treshold_silver_f = row["Silber"]
+                    dbrule.treshold_gold_f = row["Gold"]
         db.session.commit()
     except Exception as e:
         logging.warning(e)
