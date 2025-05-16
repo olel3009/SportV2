585e6c0fec5e8cfa40f9094a18cf3a25b4b879bc
SuperBeast2005 <muellerma2023@gmail.com>
2025-05-16 10:15:33 +0200

    error correction

--- api/get_jwt.py
+++ api/get_jwt.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_jwt():
-    login_url = "http://localhost:5000/login"
+    login_url = "http://localhost:5000/users/login"
     protected_url = "http://localhost:5000/protected"
     
     payload = {
@@ -11,15 +11,22 @@ def get_jwt():
 
     login_response = requests.post(login_url, json=payload)
     protected_response = requests.get(protected_url, json=payload)
-    access_token = login_response.json()["access_token"]
-    
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
 
-    response = requests.get(login_url, headers=headers)
+    try:
+        json_data = login_response.json()
+        access_token = json_data["access_token"]
+        print(access_token)
+        headers = {
+        "Authorization": f"Bearer {access_token}"
+        }
+
+        response = requests.get(login_url, headers=headers)
+
+        return response
+    except requests.exceptions.JSONDecodeError:
+        print("Exception")
     
-    return response
+    
 
 if "__main__" == __name__:
     get_jwt()
